444b3104d4a5381d1e09d71389de49b78110f9fc
Cabral2104 <valdezemilio50@gmail.com>
2024-02-27 21:44:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Archivos_XML_JSON
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContenido = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Location = new System.Drawing.Point(12, 12);
+            this.txtContenido.Multiline = true;
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(689, 426);
+            this.txtContenido.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(713, 61);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 51);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(713, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 51);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtContenido);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContenido;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+
+
+namespace Archivos_XML_JSON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // Abrir un cuadro de diálogo para seleccionar un archivo XML o JSON
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos XML (*.xml)|*.xml|Archivos JSON (*.json)|*.json";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filePath); // Leer el contenido del archivo
+
+                // Mostrar el contenido del archivo en el TextBox
+                txtContenido.Text = fileContent;
+
+                // Determinar el tipo de archivo (XML o JSON) basado en la extensión del archivo
+                if (filePath.EndsWith(".xml"))
+                {
+                    // Procesamiento de archivos XML
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(filePath); // Cargar el archivo XML en un objeto XmlDocument
+                    
+                }
+                else if (filePath.EndsWith(".json"))
+                {
+                    // Procesamiento de archivos JSON
+                    dynamic jsonContent = JsonConvert.DeserializeObject(fileContent); // Deserializar el JSON
+                    
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Abrir un cuadro de diálogo para guardar el archivo
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivos XML (*.xml)|*.xml|Archivos JSON (*.json)|*.json";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                // Guardar el contenido del TextBox en el archivo
+                File.WriteAllText(filePath, txtContenido.Text);
+                MessageBox.Show("Archivo guardado exitosamente.");
+
+                // Verificar la extensión del archivo y realizar operaciones adicionales según sea necesario
+                if (filePath.EndsWith(".xml"))
+                {
+                    // Procesamiento de archivos XML
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(filePath); // Cargar el archivo XML en un objeto XmlDocument
+                                           
+
+                    // Guardar el objeto XmlDocument en el archivo
+                    xmlDoc.Save(filePath); // Guardar el archivo XML con los cambios realizados
+                }
+                else if (filePath.EndsWith(".json"))
+                {
+                    // Procesamiento de archivos JSON
+                    string fileContent = File.ReadAllText(filePath);
+                    dynamic jsonContent = JsonConvert.DeserializeObject(fileContent); // Deserializar el JSON
+                                                                                      
+
+                    // Serializar el objeto JSON y guardarlo en el archivo
+                    string jsonOutput = JsonConvert.SerializeObject(jsonContent); // Serializar el JSON
+                    File.WriteAllText(filePath, jsonOutput); // Guardar el archivo JSON con los cambios realizados
+                }
+            }
+        }
+    }
+}
